9594a39783862c9425ee113637e365b321cf6dd8
hanna244 <shhn0509@gmail.com>
2021-03-08 01:41:08 +0900

    [변경] 네이밍 컨벤션 기준에 맞춰서 파일명 변경
    컴포넌트/레이아웃/컨테이너/페이지는 타이틀 케이스로 설정하기 정했기
    때문에 해당 기준에 맞춰서 파일명을 변경 한다.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App/App'
-import './styles/index.js'
 
 ReactDOM.render(
   <React.StrictMode>
--- src/styles/index.js
+++
@@ -1,5 +0,0 @@
-// 전역 스타일 모으기
-// normalize
-
-// import 'normalize.css'
-import './common.module.sass'
