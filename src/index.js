a71f39f0a7cd0b7a93ae38aa959a294ebeaeba95
hanna244 <shhn0509@gmail.com>
2021-03-08 18:58:12 +0900

    [스타일] 글로벌 스타일, normalize 설정
    styled-components의 글로벌 스타일과 CRA의 normalize 불러오기
    CRA의 normalize를 사용할 때는 css파일에서 불러오기가 가능해서 따로
    css파일을 생성

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App/App'
-import './index.css'
+import './styles/normalize.css'
+import { GlobalStyles } from './styles/GlobalStyles'
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyles />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
