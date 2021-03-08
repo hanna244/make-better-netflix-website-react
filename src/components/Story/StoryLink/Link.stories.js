1604bfc82cf20f760ed2d893103d51245b39eb83
hanna244 <shhn0509@gmail.com>
2021-03-08 19:03:37 +0900

    [경험] 스토리북 도전 실패
    컴포넌트를 생성할 때 스토리북을 사용하려고 했으나 실패...
    컴포넌트 설정을 구성하는 방법과, 구성한 컴포넌트를 App 파일에 불러오는
    것, 미디어쿼리를 사용하는 것. 생각보다 어려워서 일단 시간 절약을 위해
    잠시 보류.

---
+++ src/components/Story/StoryLink/Link.stories.js
@@ -0,0 +1,71 @@
+import Link from './Link'
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
+import { withKnobs, text, boolean } from '@storybook/addon-knobs'
+
+/* ------------------------------------------------------------------- */
+
+const link = {
+  // 컴포넌트 설명을 입력하면 Storybook에 카테고리 되어 표시됩니다.
+  title: '넷플릭스/Button/Link',
+  // 데코레이터 설정
+  decorators: [withKnobs],
+  // 컴포넌트 설정
+  component: Link,
+  // 전달 인자 유형 설정
+  // argTypes: {
+  //   backgroundColor: { control: 'color' },
+  // },
+  // 전달인자 설정
+  args: {
+    children: '로그인',
+  },
+  parameters: {
+    // 뷰포트 애드온 설정
+    viewport: {
+      viewports: INITIAL_VIEWPORTS,
+      // iPhoneX,XR 만 추출
+      defaultViewport: ['iphonex', 'iphonexr'],
+    },
+  },
+}
+
+export default link
+
+// 컴포넌트 템플릿
+// 함수의 복사본을 만드는 표준 JavaScript 기법
+const Template = (args) => <Link {...args} />
+
+// sm 사이즈 컴포넌트
+export const SmSize = Template.bind({})
+SmSize.storyName = 'Small'
+SmSize.args = {
+  id: 'sm-size-kwdj1',
+  size: 'sm',
+}
+SmSize.parameters = {
+  viewport: {
+    defaultViewport: 'iphonex',
+  },
+}
+
+// md 사이즈 컴포넌트
+export const MdSize = Template.bind({})
+MdSize.storyName = 'Medium'
+MdSize.args = {
+  id: 'md-size-kwdj5',
+  size: 'md',
+}
+MdSize.parameters = {
+  viewport: {
+    defaultViewport: 'iphonexr',
+  },
+}
+
+// lg 사이즈 컴포넌트
+export const LgSize = Template.bind({})
+LgSize.storyName = 'Large'
+LgSize.args = {
+  id: 'lg-size-kwdj8',
+  size: 'lg',
+  labelHidden: boolean('labelHidden', false),
+}
