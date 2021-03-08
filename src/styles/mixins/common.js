d66700aa892115e7236efa17153eaec5bd333468
hanna244 <shhn0509@gmail.com>
2021-03-08 19:02:06 +0900

    [스타일] reset 스타일 설정
    styled-components의 믹스인을 사용해서 reset 스타일 설정

---
+++ src/styles/mixins/common.js
@@ -0,0 +1,18 @@
+import { css } from 'styled-components'
+
+const resetList = css`
+  list-style: none;
+  padding-left: 0;
+  margin-left: 0;
+  margin-right: 0;
+`
+
+const resetButton = css`
+  border: 0;
+`
+
+const resetAddress = css`
+  font-style: normal;
+`
+
+export { resetList, resetButton, resetAddress }
