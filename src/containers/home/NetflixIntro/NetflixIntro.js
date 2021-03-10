afc4111adf54de0d80b13fbd0cbc19a55fe8bb19
hanna244 <shhn0509@gmail.com>
2021-03-10 17:50:56 +0900

    [질문] 리스트 useRef 사용해서 <IntroVideo>에 접근
    리스트 렌더링에서 useRef 사용해서 <IntroVideo>에 접근하고
    싶은데 계속 current에 객체 참조가 없다고 뜬다.

--- src/containers/home/NetflixIntro/NetflixIntro.js
+++ src/containers/home/NetflixIntro/NetflixIntro.js
@@ -1,25 +1,35 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import { v4 as uuid } from 'uuid'
 import { Merit } from '../../../components'
 import meritData from '../../../data/merit.json'
 
 const NetflixIntro = () => {
+  const videoRef = useRef(null)
+
+  React.useEffect(() => {
+    console.log('DOM 마운트 이후 시점')
+    console.log(videoRef)
+  })
+
   return (
     <Merit.Container>
-      {meritData.map(({ title, description, video, image, alt, direction }) => {
-        return (
-          <Merit direction={direction}>
-            <Merit.TextWrapper>
-              <Merit.Title children={title} />
-              <Merit.SubTitle children={description} />
-            </Merit.TextWrapper>
-            <Merit.MediaWrapper>
-              <Merit.IntroImage src={image} alt={alt} />
-              <Merit.IntroVideo src={video} />
-            </Merit.MediaWrapper>
-          </Merit>
-        )
-      })}
+      {meritData.map(
+        ({ title, description, video, image, alt, direction }, index) => {
+          console.log(video)
+          return (
+            <Merit direction={direction}>
+              <Merit.Wrapper>
+                <Merit.Title children={title} />
+                <Merit.SubTitle children={description} />
+              </Merit.Wrapper>
+              <Merit.Wrapper>
+                <Merit.IntroImage src={image} alt={alt} />
+                <Merit.IntroVideo ref={videoRef} src={video} />
+              </Merit.Wrapper>
+            </Merit>
+          )
+        }
+      )}
     </Merit.Container>
   )
 }
