c1e2add7623037d9cf6740f9faf5ce0439a4512e
hanna244 <shhn0509@gmail.com>
2021-03-12 16:53:32 +0900

    [구성] FAQ → faq 데이터 바인딩

--- src/containers/home/netflixFAQ/NetflixFAQ.js
+++ src/containers/home/netflixFAQ/NetflixFAQ.js
@@ -1,7 +1,21 @@
 import React from 'react'
+import { v4 as uuid } from 'uuid'
+import faqData from '../../../data/faq.json'
+import Accordion from '../../../components/Accordion/index'
 
 const NetflixFAQ = () => {
-  return <div>넷플릭스 자주 하는 질문</div>
+  return (
+    <Accordion>
+      {faqData.map(({ question, answer }) => {
+        return (
+          <Accordion.Item key={uuid()} id={uuid()}>
+            <Accordion.Head>{question}</Accordion.Head>
+            {answer[0] && <Accordion.Body>{answer[0]}</Accordion.Body>}
+            {answer[1] ? <Accordion.Body>{answer[1]}</Accordion.Body> : null}
+          </Accordion.Item>
+        )
+      })}
+    </Accordion>
+  )
 }
-
 export default NetflixFAQ
