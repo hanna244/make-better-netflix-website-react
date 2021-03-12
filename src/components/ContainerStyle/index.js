c53817312c50e2489d38845c5d284fdab8dfc52a
hanna244 <shhn0509@gmail.com>
2021-03-12 17:13:23 +0900

    [스타일] Container 스타일 컴포넌트 생성

---
+++ src/components/ContainerStyle/index.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const ContainerStyle = styled.div`
+  padding: 2rem 0;
+  margin: 0 1.8rem;
+
+  @media (min-width: 60em) {
+    padding: 5rem 0;
+    margin: 0 20rem;
+  }
+`
+export default ContainerStyle
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,7 @@
-export { default as Link } from './Link'
+export { default as Link } from './Button'
 export { default as Merit } from './Merit'
 export { default as Photo } from './Photo'
 export { default as Accordion } from './Accordion'
+
+// 스타일 컴포넌트
+export { default as ContainerStyle } from './ContainerStyle'
