d6e52840e42af92034061414fb637dbacb05f6d9
hanna244 <shhn0509@gmail.com>
2021-03-12 18:06:24 +0900

    [경험] 컴파운드 컴포넌트 displayName 적용 실패

--- src/components/Accordion/index.js
+++ src/components/Accordion/index.js
@@ -1,10 +1,28 @@
 import React from 'react'
+import { displayName } from '../../utils'
 import { Wrapper, Item, Head, Body } from './Accordion.style'
 
+// const AccordionWrapper = ({ children }) => {
+//   AccordionWrapper.displayName = `AccordionWrapper`
+//   return children
+// }
+
+// const Accordion = ({ children, ...restProps }) => {
+//   Accordion.displayName = `${displayName(Accordion)}`
+//   AccordionWrapper = (props) => <AccordionWrapper {...props} />
+//   React.children.map(props.children, (child, index) => {
+//     console.log(child.type.displayName)
+//   })
+//   return React.cloneElement(child)
+// }
+
 const Accordion = ({ children, ...restProps }) => {
+  Accordion.displayName = `${displayName(Accordion)}`
   return <Wrapper {...restProps}>{children}</Wrapper>
 }
+
 Accordion.Item = ({ children, ...restProps }) => {
+  Accordion.Item.displayName = `${displayName(Accordion.Item)}`
   return <Item {...restProps}>{children}</Item>
 }
 Accordion.Head = ({ children, ...restProps }) => {
