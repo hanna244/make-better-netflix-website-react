3f3287867c325afee23726169624999c9fda5344
hanna244 <shhn0509@gmail.com>
2021-03-12 18:05:36 +0900

    [개선] FAQ → 본문의 태그 p로 변경

--- src/containers/home/netflixFAQ/NetflixFAQ.js
+++ src/containers/home/netflixFAQ/NetflixFAQ.js
@@ -1,21 +1,41 @@
 import React from 'react'
 import { v4 as uuid } from 'uuid'
+import styled from 'styled-components'
 import faqData from '../../../data/faq.json'
 import Accordion from '../../../components/Accordion/index'
+import { ContainerStyle as FAQContainer } from '../../../components'
+
+const Heading = styled.h2`
+  margin-top: 6rem;
+  text-align: center;
+  font-weight: bold;
+  font-size: 2.4rem;
+  line-height: 1.2;
+
+  @media (min-width: 60em) {
+    margin-top: 5.4rem;
+    font-size: 4.8rem;
+  }
+`
 
 const NetflixFAQ = () => {
   return (
-    <Accordion>
-      {faqData.map(({ question, answer }) => {
-        return (
-          <Accordion.Item key={uuid()} id={uuid()}>
-            <Accordion.Head>{question}</Accordion.Head>
-            {answer[0] && <Accordion.Body>{answer[0]}</Accordion.Body>}
-            {answer[1] ? <Accordion.Body>{answer[1]}</Accordion.Body> : null}
-          </Accordion.Item>
-        )
-      })}
-    </Accordion>
+    <FAQContainer>
+      <Heading>자주 묻는 질문</Heading>
+      <Accordion>
+        {faqData.map(({ question, answer }) => {
+          return (
+            <Accordion.Item key={uuid()} id={uuid()}>
+              <Accordion.Head>{question}</Accordion.Head>
+              <Accordion.Body>
+                {answer[0] && <p>{answer[0]}</p>}
+                {answer[1] ? <p>{answer[1]}</p> : null}
+              </Accordion.Body>
+            </Accordion.Item>
+          )
+        })}
+      </Accordion>
+    </FAQContainer>
   )
 }
 export default NetflixFAQ
