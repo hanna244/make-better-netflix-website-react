fc25b3c474f258e5f12d9cfa168b45e3618705f0
hanna244 <shhn0509@gmail.com>
2021-03-12 18:27:40 +0900

    [오류 해결]컴파운드 컴포넌트 displayName 설정 해결
    이름 없는 함수(화살표 함수)식이 아닌 function 키워드를 사용해서 이름있는
    함수로 설정하면 displayName을 따로 설정하지 않아도 된다.

--- src/components/Accordion/index.js
+++ src/components/Accordion/index.js
@@ -1,34 +1,17 @@
 import React from 'react'
-import { displayName } from '../../utils'
 import { Wrapper, Item, Head, Body } from './Accordion.style'
 
-// const AccordionWrapper = ({ children }) => {
-//   AccordionWrapper.displayName = `AccordionWrapper`
-//   return children
-// }
-
-// const Accordion = ({ children, ...restProps }) => {
-//   Accordion.displayName = `${displayName(Accordion)}`
-//   AccordionWrapper = (props) => <AccordionWrapper {...props} />
-//   React.children.map(props.children, (child, index) => {
-//     console.log(child.type.displayName)
-//   })
-//   return React.cloneElement(child)
-// }
-
-const Accordion = ({ children, ...restProps }) => {
-  Accordion.displayName = `${displayName(Accordion)}`
+const Accordion = function Accordion({ children, ...restProps }) {
   return <Wrapper {...restProps}>{children}</Wrapper>
 }
 
-Accordion.Item = ({ children, ...restProps }) => {
-  Accordion.Item.displayName = `${displayName(Accordion.Item)}`
+Accordion.Item = function AccordionItem({ children, ...restProps }) {
   return <Item {...restProps}>{children}</Item>
 }
-Accordion.Head = ({ children, ...restProps }) => {
+Accordion.Head = function AccordionHead({ children, ...restProps }) {
   return <Head {...restProps}>{children}</Head>
 }
-Accordion.Body = ({ children, ...restProps }) => {
+Accordion.Body = function AccordionBody({ children, ...restProps }) {
   return <Body {...restProps}>{children}</Body>
 }
 
