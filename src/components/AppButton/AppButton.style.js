d6252dd91325e7c7f3cbe77acd10c5ccd99f1846
hanna244 <shhn0509@gmail.com>
2021-03-13 23:30:05 +0900

    [구성] Button, LinkButton 스타일 컴포넌트 생성
    Button, LinkButton 컴포넌트 생성

---
+++ src/components/AppButton/AppButton.style.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+import { resetButton } from '../../styles/common'
+
+const AppButton = styled.button`
+  ${resetButton}
+`
+
+export default AppButton
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,8 @@
-export { default as Link } from './Button'
 export { default as Merit } from './Merit'
 export { default as Photo } from './Photo'
 export { default as Accordion } from './Accordion'
 
 // 스타일 컴포넌트
-export { default as ContainerStyle } from './ContainerStyle'
+export { default as AppContainer } from './AppContainer'
+export { default as AppButton } from './AppButton/AppButton.style'
+export { default as AppLinkButton } from './AppButton/AppLinkButton.style'
