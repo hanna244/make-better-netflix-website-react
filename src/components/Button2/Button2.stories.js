a79c66d1f4a6d388262dc6f2e39bd9a7d8b3c339
hanna244 <shhn0509@gmail.com>
2021-03-18 05:56:01 +0900

    [학습] Story:Button → 컴포넌트 경로 설정

--- src/components/Button2/Button2.stories.js
+++ src/components/Button2/Button2.stories.js
@@ -2,10 +2,10 @@ import React from 'react'
 import Button2 from './Button2'
 
 export default {
-  title: 'Button2',
+  title: 'Form/Button2',
   component: Button2,
 }
 
-export const primary = () => <Button2 primary>Primary</Button2>
+export const Primary = () => <Button2 primary>Primary</Button2>
 export const RedButton = () => <Button2 red>Red</Button2>
 export const GreenButton = () => <Button2 green>Green</Button2>
