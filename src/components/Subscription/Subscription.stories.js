f09d00250f28c524d48a948adb66467634062756
hanna244 <shhn0509@gmail.com>
2021-03-18 05:59:12 +0900

    [학습] Story → Story 내부에 Story 불러오기

---
+++ src/components/Subscription/Subscription.stories.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Primary } from '../Button2/Button2.stories'
+import { Medium } from '../Input/Input.stories'
+
+export default {
+  title: 'Form/container/Subscription',
+}
+
+export const PrimarySubscription = () => {
+  return (
+    <>
+      <Primary />
+      <Medium />
+    </>
+  )
+}
