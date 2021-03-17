a126cff90425ea91d41dcd49f26e164daedf4097
hanna244 <shhn0509@gmail.com>
2021-03-18 05:58:19 +0900

    [학습] Story → storyname 설정

--- src/components/Input/Input.stories.js
+++ src/components/Input/Input.stories.js
@@ -9,3 +9,6 @@ export default {
 export const Small = () => <Input size="small" placeholder="Small size" />
 export const Medium = () => <Input size="medium" placeholder="Medium size" />
 export const Large = () => <Input size="large" placeholder="Large size" />
+
+// Storybook에 보여지는 파일 이름 변경
+Small.storyName = 'Small Input'
