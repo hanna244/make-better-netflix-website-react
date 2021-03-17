94b4627378f0e5b009082e49b68a54c4d9804d4f
hanna244 <shhn0509@gmail.com>
2021-03-18 06:10:15 +0900

    [학습] Story → Template 사용하기

--- src/components/Input/Input.stories.js
+++ src/components/Input/Input.stories.js
@@ -12,3 +12,23 @@ export const Large = () => <Input size="large" placeholder="Large size" />
 
 // Storybook에 보여지는 파일 이름 변경
 Small.storyName = 'Small Input'
+
+const Template = (args) => <Input {...args} />
+
+export const SmallA = Template.bind({})
+SmallA.args = {
+  size: 'small',
+  placeholder: 'SmallA',
+}
+
+export const SmallB = Template.bind({})
+SmallB.args = {
+  ...SmallA.args,
+  placeholder: 'SmallB',
+}
+
+export const mediumA = Template.bind({})
+mediumA.args = {
+  size: 'medium',
+  placeholder: 'mediumA',
+}
