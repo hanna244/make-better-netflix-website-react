1a91978ee1f41cc723b0e5fdab63aeb5cc1d34bc
hanna244 <shhn0509@gmail.com>
2021-03-18 17:06:53 +0900

    [변경] 학습 story 파일 폴더 위치 변경

--- src/components/story_practice/Subscription/Subscription.stories.js
+++ src/components/story_practice/Subscription/Subscription.stories.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Primary } from '../Button2/Button2.stories'
-import { Medium } from '../Input/Input.stories'
+import { Primary } from '../../Button2/Button2.stories'
+import { Medium } from '../../Input/Input.stories'
 
 export default {
   title: 'Form/container/Subscription',
