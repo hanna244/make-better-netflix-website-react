1eb7c5aed610528bb82fc4b5875b1af37cb21807
hanna244 <shhn0509@gmail.com>
2021-03-19 07:17:30 +0900

    [개선] Accordion → close 상태 시 dd 요소 제거
    문제 : answer 기본 값이 있어서 close 상태에도 여전히 <dd>요소가 있는 것을 확인
    해결: showAnswer을 적용해서 해결

--- src/components/Accordion/Accordion.js
+++ src/components/Accordion/Accordion.js
@@ -1,17 +1,24 @@
 import React from 'react'
 import { Item, Head, Body } from './Accordion.style'
+import { v4 as uuid } from 'uuid'
 
-const Accordion = ({ question, answer = [], ...restProps }) => {
+const Accordion = ({
+  id,
+  question,
+  showAnswer = false,
+  answer,
+  ...restProps
+}) => {
   return (
-    <ul {...restProps}>
-      <Item>
-        <Head>{question}</Head>
+    <Item key={uuid()} id={uuid()} {...restProps}>
+      <Head>{question}</Head>
+      {showAnswer ? (
         <Body>
           <p>{answer[0] ?? null}</p>
           <p>{answer[1] ?? null}</p>
         </Body>
-      </Item>
-    </ul>
+      ) : null}
+    </Item>
   )
 }
 
