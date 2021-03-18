64123a70e1fa602ccc6eb24d4f0b220fda86a14f
hanna244 <shhn0509@gmail.com>
2021-03-18 18:51:40 +0900

    [구성] Button → storybook-addon-designs 적용

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -1,8 +1,17 @@
 import Button from './Button'
+import { withDesign } from 'storybook-addon-designs'
 
 export default {
   title: 'From/Button',
   component: Button,
+  decorators: [withDesign],
+  parameters: {
+    design: {
+      type: 'figma',
+      url:
+        'https://www.figma.com/file/OxqbFId2ufYIqjEimukwSD/%EB%84%B7%ED%94%8C%EB%A6%AD%EC%8A%A4(Netflix)?node-id=125%3A96',
+    },
+  },
 }
 
 const Template = (args) => <Button {...args} />
