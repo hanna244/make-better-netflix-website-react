3bcbec9f919c9b3249d8aa323d65e313d00a7b65
hanna244 <shhn0509@gmail.com>
2021-03-19 21:44:41 +0900

    [구성] Frame → 컴포넌트 생성

---
+++ src/components/Frame/Frame.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import device from './image/category=Device.png'
+import download from './image/category=Download.png'
+import tv from './image/category=TV.png'
+
+const Frame = ({ alt = '', type, ...restProps }) => {
+  let src = ''
+  switch (type) {
+    case device:
+      src = 'device'
+      break
+    case download:
+      src = 'download'
+      break
+    case tv:
+      src = 'tv'
+      break
+
+    default:
+      throw new Error('지원하는 이미지가 아닙니다.')
+  }
+  return <img src={src} alt={alt} {...restProps} />
+}
+
+export default Frame
---
+++ src/components/Frame/Frame.stories.js
@@ -0,0 +1,23 @@
+import Frame from './Frame'
+import { withDesign } from 'storybook-addon-designs'
+
+export default {
+  title: 'Components/UI/Frame',
+  component: Frame,
+  decorators: [withDesign],
+  parameters: {
+    design: {
+      type: 'figma',
+      url:
+        'https://www.figma.com/file/OxqbFId2ufYIqjEimukwSD/%EB%84%B7%ED%94%8C%EB%A6%AD%EC%8A%A4(Netflix)?node-id=125%3A134',
+    },
+  },
+}
+
+const Template = (args) => <Frame {...args} />
+
+export const DeviceFrame = Template.bind({})
+DeviceFrame.args = {
+  alt: '',
+}
+DeviceFrame.storyName = '프레임(Device)'
