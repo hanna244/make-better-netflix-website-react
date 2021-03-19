5ff321f561d8e9d1aa9c8ea85dfcd8c40e2f609a
hanna244 <shhn0509@gmail.com>
2021-03-19 20:45:28 +0900

    [스타일] Background → dimmed 상태 배경이미지 스타일 설정

--- src/components/Background/Background.style.js
+++ src/components/Background/Background.style.js
@@ -1,7 +1,13 @@
 import styled from 'styled-components'
 
 export const BackgroundStyle = styled.div`
-  background: url('./assets/Cover.jpg') no-repeat center center;
+  background: linear-gradient(
+      180deg,
+      #000000 5.68%,
+      rgba(0, 0, 0, 0.3) 42.95%,
+      rgba(0, 0, 0, 0.8) 79.3%
+    ),
+    url('./assets/Cover.jpg') no-repeat center center;
   /* storybook에 보이게 하기 위해 임의로 높이 값 설정  */
   height: 261vh;
 `
