20eb8a0c8cf9de3f2cdd89cecdad11786ab71580
hanna244 <shhn0509@gmail.com>
2021-03-21 00:51:30 +0900

    [구성] SelectIcon → figma 연결

--- src/components/SelectIcon/SelectIcon.stories.js
+++ src/components/SelectIcon/SelectIcon.stories.js
@@ -9,7 +9,7 @@ export default {
     design: {
       type: 'figma',
       url:
-        'https://www.figma.com/file/OxqbFId2ufYIqjEimukwSD/%EB%84%B7%ED%94%8C%EB%A6%AD%EC%8A%A4(Netflix)?node-id=125%3A96',
+        'https://www.figma.com/file/OxqbFId2ufYIqjEimukwSD/%EB%84%B7%ED%94%8C%EB%A6%AD%EC%8A%A4(Netflix)?node-id=1%3A61',
     },
   },
 }
