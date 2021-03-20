8057da025ed1b7b17ca68c89f87e4b609e56f4d2
hanna244 <shhn0509@gmail.com>
2021-03-21 00:52:05 +0900

    [구성] Favicon → 컴포넌트 생성

---
+++ src/components/Favicon/Favicon.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Favicon = (props) => {
+  return <img src="./assets/Favicon.png" alt="넷플릭스 아이콘" {...props} />
+}
+
+export default Favicon
---
+++ src/components/Favicon/Favicon.stories.js
@@ -0,0 +1,20 @@
+import Favicon from './Favicon'
+import { withDesign } from 'storybook-addon-designs'
+
+export default {
+  title: 'Components/UI/Favicon',
+  component: Favicon,
+  decorators: [withDesign],
+  parameters: {
+    design: {
+      type: 'figma',
+      url:
+        'https://www.figma.com/file/OxqbFId2ufYIqjEimukwSD/%EB%84%B7%ED%94%8C%EB%A6%AD%EC%8A%A4(Netflix)?node-id=1%3A54',
+    },
+  },
+}
+
+const Template = (args) => <Favicon {...args} />
+
+export const Favi = Template.bind({})
+Favi.storyName = '넷플릭스 아이콘(favicon)'
