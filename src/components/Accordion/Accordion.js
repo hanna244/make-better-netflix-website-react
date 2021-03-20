6a46e97f020e978344fff74eee7404277c2ec50e
hanna244 <shhn0509@gmail.com>
2021-03-21 07:06:42 +0900

    [개선] Accordion → isOpen 속성을 initState에 설정

--- src/components/Accordion/Accordion.js
+++ src/components/Accordion/Accordion.js
@@ -3,11 +3,6 @@ import { string, bool } from 'prop-types'
 import { Item, Head, Body, OpenButton, PlusImg } from './Accordion.style'
 import { v4 as uuid } from 'uuid'
 
-const initialState = {
-  bodyClassName: '',
-  isShow: false,
-}
-
 function reducer(state, action) {
   switch (action.type) {
     case 'openBodyAndAnimation':
@@ -22,10 +17,11 @@ function reducer(state, action) {
 }
 
 const Accordion = ({ question, isOpen, answer, ...restProps }) => {
-  const [{ bodyClassName, isShow }, dispatch] = useReducer(
-    reducer,
-    initialState
-  )
+  const [{ bodyClassName, isShow }, dispatch] = useReducer(reducer, {
+    bodyClassName: '',
+    isShow: isOpen,
+  })
+
   const openAccordion = () => {
     dispatch({ type: 'openBodyAndAnimation' })
   }
@@ -33,10 +29,8 @@ const Accordion = ({ question, isOpen, answer, ...restProps }) => {
     dispatch({ type: 'closeAnimation' })
     window.setTimeout(() => {
       dispatch({ type: 'closeBody' })
-    }, 200)
+    }, 400)
   }
-
-  // const handleOpenCloseAccordion =
 
   const handleAccordion = useMemo(
     () => (accordionState) => {
@@ -52,12 +46,7 @@ const Accordion = ({ question, isOpen, answer, ...restProps }) => {
 
   return (
     <Item
-      // 너무 많은 리렌더링 오류의 원인은 이곳에 있다.
-      // ---- 원인
-      // handleAccordion(isOpen) 구문은 함수 실행 구문이기 때문에 React 요소가 렌더링 될 때마다 해당 함수가 실행되어 성능에 문제를 야기한다.
-      // 이를 해결하기 위해 해당 이벤트가 실행 될 때만 handleAccordion 함수를 실행하도록 callback을 사용해야한다.
-      // ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
-      onClick={() => handleAccordion(isOpen)}
+      onClick={() => handleAccordion(isShow)}
       key={uuid()}
       id={uuid()}
       {...restProps}
