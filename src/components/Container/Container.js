83d6752b71714f50589a659503363b850ddba051
Hanna Kim <shhn0509@gmail.com>
2021-03-25 06:31:41 +0900

    [구성] Container → Template 생성

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -5,15 +5,30 @@ import { ContainerStyle } from './Container.style'
 const Container = ({
   paddingVertical,
   marginHorizon,
+  align,
+  style,
   children,
   ...restProps
 }) => {
-  return <ContainerStyle {...restProps}>{children}</ContainerStyle>
+  const styles = {
+    textAlign: align,
+    ...style,
+  }
+
+  return (
+    <ContainerStyle
+      style={styles}
+      marginHorizon={marginHorizon}
+      paddingVertical={paddingVertical}
+      {...restProps}
+    >
+      {children}
+    </ContainerStyle>
+  )
 }
 
 Container.defaultProps = {
   children: '컨텐츠를 넣어주세요',
-  // dufaultProps에 설정한 Logo 컴포넌트는 왜 문자열로 표시되나요?
   // children: <Logo width="350" />,
 }
 
--- src/components/Container/Container.stories.js
+++ src/components/Container/Container.stories.js
@@ -12,8 +12,28 @@ const Template = (args) => <Container {...args} />
 
 export const NormalContainer = Template.bind({})
 NormalContainer.args = {
-  paddingVertical: 'auto',
   marginHorizon: 'auto',
   children: <Logo width="350" />,
 }
 NormalContainer.storyName = '컨테이너 버튼(normal)'
+
+export const CenterContainer = Template.bind({})
+CenterContainer.args = {
+  ...NormalContainer.args,
+  align: 'center',
+}
+CenterContainer.storyName = '컨테이너 버튼(center)'
+
+export const LeftContainer = Template.bind({})
+LeftContainer.args = {
+  ...NormalContainer.args,
+  align: 'left',
+}
+LeftContainer.storyName = '컨테이너 버튼(left)'
+
+export const RightContainer = Template.bind({})
+RightContainer.args = {
+  ...NormalContainer.args,
+  align: 'right',
+}
+RightContainer.storyName = '컨테이너 버튼(right)'
