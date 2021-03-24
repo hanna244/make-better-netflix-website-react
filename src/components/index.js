60f9990c21a7e8c6fab3e618d2c41f6e6c500b8f
Hanna Kim <shhn0509@gmail.com>
2021-03-24 18:41:06 +0900

    [변경] conponent 앤트리 파일에 스타일 컴포넌트 추가

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
+/* 컴포넌트 --------------------------------------------------------------------- */
 export { default as Accordion } from './Accordion/Accordion'
 export { default as Background } from './Background/Background'
 export { default as Button } from './Button/Button'
@@ -7,3 +8,6 @@ export { default as Frame } from './Frame/Frame'
 export { default as Logo } from './Logo/Logo'
 export { default as PromotionButton } from './PromotionButton/PromotionButton'
 export { default as SelectIcon } from './SelectIcon/SelectIcon'
+
+/* 스타일드 컴포넌트 ---------------------------------------------------------------- */
+export { ButtonStyle } from './Button/Button.style'
