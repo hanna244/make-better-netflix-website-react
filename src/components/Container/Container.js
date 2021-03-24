e1c3c174f19c342c3e2256b78a336036243a8ed4
Hanna Kim <shhn0509@gmail.com>
2021-03-25 05:55:38 +0900

    [문제] defaultProps에 컴포넌트를 설정했으나 문자열로 표시되는 문제

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { ContainerStyle } from './Container.style'
+// import Logo from '../Logo/Logo'
 
 const Container = ({
   paddingVertical,
@@ -12,6 +13,8 @@ const Container = ({
 
 Container.defaultProps = {
   children: '컨텐츠를 넣어주세요',
+  // dufaultProps에 설정한 Logo 컴포넌트는 왜 문자열로 표시되나요?
+  // children: <Logo width="350" />,
 }
 
 export default Container
