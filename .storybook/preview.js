1dd9fac87a5b53e56d555a673952a06905f47b3d
Hanna Kim <shhn0509@gmail.com>
2021-03-25 18:11:26 +0900

    [설정] Story → customViewports 설정

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -12,16 +12,6 @@ import {
   docs,
 } from './config'
 
-export const parameters = {
-  layout: 'fullscreen',
-  backgrounds,
-  viewport,
-  controls,
-  a11y,
-  actions,
-  docs,
-}
-
 export const globalTypes = localization
 
 // 클로벌 스타일 적용
@@ -33,3 +23,40 @@ export const decorators = [
     </>
   ),
 ]
+
+const customViewports = {
+  netflixSmall: {
+    name: 'netflix small',
+    styles: {
+      width: '640px',
+      height: '960px',
+    },
+  },
+  netflixLarge: {
+    name: 'netflix large',
+    styles: {
+      width: '960px',
+      height: '1440px',
+    },
+  },
+
+  netflixXLarge: {
+    name: 'netflix X large',
+    styles: {
+      width: '1440px',
+      height: '960px',
+    },
+  },
+}
+
+export const parameters = {
+  // padding: 1rem 제거
+  layout: 'fullscreen',
+  // 커스텀 뷰포드 설정
+  viewport: { viewports: customViewports },
+  backgrounds,
+  controls,
+  a11y,
+  actions,
+  docs,
+}
