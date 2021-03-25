8d148ed385af177fc826df24a2ed177b895a4cbe
Hanna Kim <shhn0509@gmail.com>
2021-03-25 18:18:28 +0900

    [구성] Header → 스타일 설정

--- src/layout/header/Header.js
+++ src/layout/header/Header.js
@@ -1,28 +1,32 @@
 import React from 'react'
-import { Logo } from '../../components'
+
 import {
+  OuterContainer,
+  InnerContainer,
   HeaderHomeLink,
-  HeaderContainer,
   HeaderLoginLink,
+  HeaderLogo,
 } from './Header.style'
 
-const Header = () => {
+const Header = ({ className, ...restProps }) => {
   // 작업 중 링크 기본 이벤트 제어 설정
   const controlLink = (e) => {
     e.preventDefault()
   }
 
   return (
-    <HeaderContainer
-      paddingVertical="4rem"
-      marginHorizon="12rem"
-      onClick={controlLink}
-    >
-      <HeaderHomeLink href="/">
-        <Logo />
-      </HeaderHomeLink>
-      <HeaderLoginLink href="login" tag="a" />
-    </HeaderContainer>
+    <OuterContainer>
+      <InnerContainer
+        onClick={controlLink}
+        className={className}
+        {...restProps}
+      >
+        <HeaderHomeLink href="/">
+          <HeaderLogo />
+        </HeaderHomeLink>
+        <HeaderLoginLink href="login" tag="a" />
+      </InnerContainer>
+    </OuterContainer>
   )
 }
 
--- src/layout/header/Header.style.js
+++ src/layout/header/Header.style.js
@@ -1,11 +1,27 @@
 import styled from 'styled-components'
-import { Button, Container } from '../../components'
+import { Button, Container, Logo } from '../../components'
 import { resetLink } from '../../styles/common'
 
-export const HeaderContainer = styled(Container)``
+export const OuterContainer = styled(Container)`
+  padding: 4rem 0;
+  margin: 0 12rem;
+`
+export const InnerContainer = styled(Container)`
+  display: flex;
+  justify-content: space-between;
+  max-width: 120rem;
+`
+
 export const HeaderHomeLink = styled.a`
   ${resetLink}
+  display: block;
+  width: 7.4rem;
+  height: auto;
 `
+export const HeaderLogo = styled(Logo)`
+  width: 14rem;
+`
+
 export const HeaderLoginLink = styled(Button)`
   ${resetLink}
 `
