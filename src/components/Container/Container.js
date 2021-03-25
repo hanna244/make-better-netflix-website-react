b2aab474da4295b99fdf4ac3a2e93961958c4a8b
Hanna Kim <shhn0509@gmail.com>
2021-03-25 17:16:11 +0900

    [개선] Container → 설정 된 속성 일부 제거
    paddingVertical, marginHorizon 속성 제거, 스타일 확장 시 해당 속성을
    요소의 props로 전달하는 것 보다 스타일 속성으로 전달하는 것이 편함.

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -3,26 +3,14 @@ import { ContainerStyle } from './Container.style'
 // import Logo from '../Logo/Logo'
 import { object, oneOf, string } from 'prop-types'
 
-const Container = ({
-  paddingVertical,
-  marginHorizon,
-  align,
-  style,
-  children,
-  ...restProps
-}) => {
+const Container = ({ align, style, children, ...restProps }) => {
   const styles = {
     textAlign: align,
     ...style,
   }
 
   return (
-    <ContainerStyle
-      style={styles}
-      marginHorizon={marginHorizon}
-      paddingVertical={paddingVertical}
-      {...restProps}
-    >
+    <ContainerStyle style={styles} {...restProps}>
       {children}
     </ContainerStyle>
   )
@@ -30,16 +18,11 @@ const Container = ({
 
 Container.defaultProps = {
   children: '컨텐츠를 넣어주세요',
-  marginHorizon: 'auto',
-  paddingVertical: '0px',
+
   // children: <Logo width="350" />,
 }
 
 Container.propTypes = {
-  /** padding-top/bouttom 값을 동시에 설정할 수 있습니다. */
-  paddingVertical: string,
-  /** margin-left/right 값을 동시에 설정할 수 있습니다. */
-  marginHorizon: string,
   /** 컨텐츠 정렬을 설정 할 수 있습니다. */
   align: oneOf(['center', 'left', 'right']),
   /** 사용자 정의 스타일을 객체 타입으로 설정할 수 있습니다. */
