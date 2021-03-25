1fb99380cecdeca8ba780f8120f3e99ee4cd6e48
Hanna Kim <shhn0509@gmail.com>
2021-03-25 18:19:41 +0900

    [문제] 미디어 쿼리 변수 적용이 안됨

--- src/layout/header/Header.style.js
+++ src/layout/header/Header.style.js
@@ -1,15 +1,29 @@
 import styled from 'styled-components'
 import { Button, Container, Logo } from '../../components'
 import { resetLink } from '../../styles/common'
+import { device } from '../../config/responsiveConfig'
 
 export const OuterContainer = styled(Container)`
   padding: 4rem 0;
   margin: 0 12rem;
+
+  @media ${device.md} {
+    padding: 2.3rem 0;
+    margin: 0 1.8rem;
+    max-width: 120rem;π
+  }
 `
 export const InnerContainer = styled(Container)`
   display: flex;
   justify-content: space-between;
   max-width: 120rem;
+
+  /* @media (min-width: 60em) { */
+  @media ${device.md} {
+    padding: 0;
+    margin: 0 1.8rem;
+    max-width: 120rem;
+  }
 `
 
 export const HeaderHomeLink = styled.a`
@@ -17,6 +31,11 @@ export const HeaderHomeLink = styled.a`
   display: block;
   width: 7.4rem;
   height: auto;
+
+  /* @media (min-width: 60em) { */
+  @media ${device.md} {
+    width: 14rem;
+  }
 `
 export const HeaderLogo = styled(Logo)`
   width: 14rem;
