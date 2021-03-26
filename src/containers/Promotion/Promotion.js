01dad3e96f9474b9eb2455bd3b9ef1325d0e6560
Hanna Kim <shhn0509@gmail.com>
2021-03-27 07:18:54 +0900

    [문제] TypeError 발생
    상황: 조건문 코드에서 문제가 생겼다고 한다.
    원인: 아직 알 수 없음.
    오류 메세지: TypeError: Object(...) is not a function

--- src/containers/Promotion/Promotion.js
+++ src/containers/Promotion/Promotion.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Input, PromotionButton } from '../../components'
+import { isValidEmail, passwordRegEx } from '../../utils'
 import {
   PromotionContainer,
   PromotionInput,
@@ -9,7 +9,21 @@ import {
 const Promotion = () => {
   // Input 컴포넌트에서 value 값을 가져오기 위해 사용
   const [inputValue, setInputValue] = useState('')
-  console.log(inputValue)
+  const [inputAttr, setInputAttr] = useState({})
+
+  const handleInputIsValid = () => {
+    if (inputValue.trim().length !== 0) {
+      setInputAttr({ valid: `valid="true"` })
+    } else {
+      setInputAttr({ valid: `valid="false"` })
+    }
+
+    if (!passwordRegEx(inputValue)) {
+      setInputAttr({ invalid: `invalid="false"` })
+    } else {
+      setInputAttr({ invalid: `invalid="true"` })
+    }
+  }
 
   return (
     <PromotionContainer>
@@ -19,6 +33,8 @@ const Promotion = () => {
         label="이메일 주소"
         errorMessege="정확한 이메일 주소를 입력하세요."
         onInputComplete={(value) => setInputValue(value)}
+        onBlur={handleInputIsValid}
+        {...inputAttr}
       />
       <SignUpButton label="30일 무료 이용" />
     </PromotionContainer>
