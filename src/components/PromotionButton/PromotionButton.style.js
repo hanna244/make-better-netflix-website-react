c840708c38f87801485fa651d8423dc621ac926e
Hanna Kim <shhn0509@gmail.com>
2021-03-26 19:06:54 +0900

    [스타일] PromotionButton → font-weight 추가

--- src/components/PromotionButton/PromotionButton.style.js
+++ src/components/PromotionButton/PromotionButton.style.js
@@ -6,6 +6,7 @@ import { ButtonStyle } from '../Button/Button.style'
 export const PromotionButtonStyle = styled(ButtonStyle)`
   padding: 0.9375em 1.25em;
   border-radius: 0;
+  font-weight: normal;
   font-size: 2.2rem;
   line-height: 1.35;
   letter-spacing: -0.04em;
