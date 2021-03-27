b12c833ffbd6fb40261b4e2e8ffd05d3d54282a5
Hanna Kim <shhn0509@gmail.com>
2021-03-27 21:37:06 +0900

    [문제] LinkListStyle에 미디어 쿼리 사용 시 스타일 적용 안됨
    상황 grid-column의 개수를 미디어 쿼리를 사용해서 변경하려 했으나
    미디어 쿼리 사용 시 적용이 안됨

--- src/layout/footer/Footer.style.js
+++ src/layout/footer/Footer.style.js
@@ -1,24 +1,38 @@
 import styled from 'styled-components'
-import { resetList } from '../../styles/common'
+import { resetList, resetPara } from '../../styles/common'
 import { getColor } from '../../utils'
 import { Definition } from './compound_components'
 
 export const FooterStyle = styled.footer`
   color: ${getColor('gray')};
-`
-export const DefinitionStyle = styled(Definition)`
+
   p {
+    ${resetPara}
+    display:inline-block;
     margin-top: 4rem;
+    font-weight: bold;
+    font-size: 2rem;
+    line-height: 1.4;
 
     @media (min-width: 60em) {
       margin-top: 5rem;
       line-height: 1.2;
     }
   }
+  .linkBreak {
+    @media (min-width: 60em) {
+      display: none;
+    }
+  }
 `
+export const DefinitionStyle = styled(Definition)``
 
 export const LinkListStyle = styled.ul`
   ${resetList}
   display: grid;
-  grid-template-columns: repeat(2, auto);
+  grid-template-columns: repeat(2, minmax(0, 16rem));
+
+  @media (min-width: 60em) {
+    grid-template-columns: repeat(4, 1ft);
+  }
 `
