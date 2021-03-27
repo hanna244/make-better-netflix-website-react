a19f7a57de615b5318bf431258df6d2231d265fa
Hanna Kim <shhn0509@gmail.com>
2021-03-27 22:05:43 +0900

    [스타일] Footer → LinkListStyle, AddressHead 스타일 적용

--- src/layout/footer/Footer.style.js
+++ src/layout/footer/Footer.style.js
@@ -31,8 +31,37 @@ export const LinkListStyle = styled.ul`
   ${resetList}
   display: grid;
   grid-template-columns: repeat(2, minmax(0, 16rem));
+  grid-gap: 1.3rem;
+  margin-top: 3rem;
+  font-size: 1.3rem;
+  line-height: 1.2;
 
   @media (min-width: 60em) {
     grid-template-columns: repeat(4, 1fr);
+    grid-gap: 1.6rem;
+    margin-top: 3.5rem;
+    font-size: 1.6rem;
+  }
+`
+
+export const AddressHead = styled.h4`
+  margin: 3rem 0 0;
+  font-weight: bold;
+  font-size: 1.6rem;
+  line-height: 1.2;
+
+  @media (min-width: 60em) {
+    margin: 4rem 0 0;
+    font-size: 2.2rem;
+  }
+`
+export const AddressStyle = styled.address`
+  margin-top: 2rem;
+  font-style: normal;
+  font-size: 1.3rem;
+  line-height: 2;
+
+  @media (min-width: 60em) {
+    margin-top: 1.6rem;
   }
 `
