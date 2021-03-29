d7719faf7c45e944bf0bba9c307120226dcc146a
Hanna Kim <shhn0509@gmail.com>
2021-03-29 20:45:16 +0900

    [스타일] LinkListItem → grid 스타일 제거

--- src/layout/footer/compound_components/LinkListItem/LinkListItem.style.js
+++ src/layout/footer/compound_components/LinkListItem/LinkListItem.style.js
@@ -3,7 +3,6 @@ import { resetLink } from '../../../../styles/common'
 import { getColor } from '../../../../utils'
 
 export const LinkItemStyle = styled.li`
-  display: grid;
   color: ${getColor('gray')};
 
   a {
