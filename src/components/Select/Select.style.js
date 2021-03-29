055f0ea1b4cd682dd540b5c4f7a527c1ea71ce08
Hanna Kim <shhn0509@gmail.com>
2021-03-29 11:11:18 +0900

    [문제] Select → <option>에 font 스타일 적용 안됨

--- src/components/Select/Select.style.js
+++ src/components/Select/Select.style.js
@@ -1,7 +1,17 @@
 import styled from 'styled-components'
+import { getColor } from '../../utils'
 
 export const SelectStyle = styled.select`
+  /* 브라우저 기본 디자인 적용 안 함 */
   -moz-appearance: none;
   -webkit-appearance: none;
   appearance: none;
+  padding: 1em 2.12em;
+  background: url('./assets/select_global.svg') no-repeat left 1rem;
+`
+
+export const OptionStyle = styled.option`
+  color: ${getColor('gray')};
+  font-size: 1.6rem;
+  line-height: 1.2;
 `
