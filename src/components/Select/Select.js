52809f47dfbf740e9d4f729a947d80b5d1201543
Hanna Kim <shhn0509@gmail.com>
2021-03-29 12:06:00 +0900

    [경험] includes()로 조건 처리 시도 하지만 메서드 사용 시 오류

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import { v4 as uuid } from 'uuid'
-import { array, string, number } from 'prop-types'
+import { array, string } from 'prop-types'
 import { SelectStyle, OptionStyle } from './Select.style'
 
-const Select = ({ selected = "3", id, option, ...restProps }) => {
+const Select = ({ selected = "중국어", id, option, ...restProps }) => {
   return (
     <>
       <label htmlFor={id}></label>
       <SelectStyle id={id} {...restProps}>
-        {option.map((item, index) => (
-          {index === selected ? <OptionStyle selected value={item.toLowerCase()} key={uuid()} id={uuid()}>
+        {option.map((item) => (
+          {item?.includes(selected) ? <OptionStyle selected value={item.toLowerCase()} key={uuid()} id={uuid()}>
           {item}
         </OptionStyle> : <OptionStyle value={item.toLowerCase()} key={uuid()} id={uuid()}>
         {item}
@@ -23,7 +23,7 @@ const Select = ({ selected = "3", id, option, ...restProps }) => {
 Select.propTypes = {
   /** 셀렉트의 옵션 값을 설정 할 수 있습니다. */
   option: array.isRequired,
-  selected: number,
+  selected: string,
   /**  */
   id: string.isRequired,
 }
