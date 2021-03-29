7ded51be1f6516ce60c1b28fe8eb54c661151559
Hanna Kim <shhn0509@gmail.com>
2021-03-29 20:41:50 +0900

    [경험] includes() 사용 시 오류 해결 시도 → 실패

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -2,19 +2,22 @@ import React from 'react'
 import { v4 as uuid } from 'uuid'
 import { array, string } from 'prop-types'
 import { SelectStyle, OptionStyle } from './Select.style'
+import { a11yHidden } from '../../styles/common'
 
-const Select = ({ selected = "중국어", id, option, ...restProps }) => {
+const Select = ({ label, selected = '', id, option, ...restProps }) => {
+  console.log(option)
   return (
     <>
-      <label htmlFor={id}></label>
+      <label style={{ a11yHidden }} htmlFor={id}>
+        {label}
+      </label>
       <SelectStyle id={id} {...restProps}>
         {option.map((item) => (
+          // includes() 메서드 사용 불가 
           {item?.includes(selected) ? <OptionStyle selected value={item.toLowerCase()} key={uuid()} id={uuid()}>
           {item}
         </OptionStyle> : <OptionStyle value={item.toLowerCase()} key={uuid()} id={uuid()}>
-        {item}
-      </OptionStyle>}
-        ))}
+        {item}</OptionStyle>}
       </SelectStyle>
     </>
   )
@@ -22,9 +25,12 @@ const Select = ({ selected = "중국어", id, option, ...restProps }) => {
 
 Select.propTypes = {
   /** 셀렉트의 옵션 값을 설정 할 수 있습니다. */
-  option: array.isRequired,
+  option: array,
+  /** 기본으로 선택되는 옵션을 지정할 수 있습니다. */
   selected: string,
-  /**  */
+  /** 접근성을 위해 select의 레이블을 설정해야 합니다. 설정한 레이블은 숨김 처리 됩니다(CSS) */
+  label: string.isRequired,
+  /** 접근성을 위해 select의 레이블의 고유 id를 설정해야 합니다. */
   id: string.isRequired,
 }
 
