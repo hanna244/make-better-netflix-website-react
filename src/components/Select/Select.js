a4019d1341b422ed601a979a7a82db844189c5af
Hanna Kim <shhn0509@gmail.com>
2021-03-29 11:50:50 +0900

    [문제] Select → index를 사용한 조건 처리 시 오류

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,14 +1,19 @@
 import React from 'react'
-import { array, string } from 'prop-types'
-import { SelectStyle } from './Select.style'
+import { v4 as uuid } from 'uuid'
+import { array, string, number } from 'prop-types'
+import { SelectStyle, OptionStyle } from './Select.style'
 
-const Select = ({ id, option, ...restProps }) => {
+const Select = ({ selected = "3", id, option, ...restProps }) => {
   return (
     <>
       <label htmlFor={id}></label>
       <SelectStyle id={id} {...restProps}>
-        {option.map((item) => (
-          <option>{item}</option>
+        {option.map((item, index) => (
+          {index === selected ? <OptionStyle selected value={item.toLowerCase()} key={uuid()} id={uuid()}>
+          {item}
+        </OptionStyle> : <OptionStyle value={item.toLowerCase()} key={uuid()} id={uuid()}>
+        {item}
+      </OptionStyle>}
         ))}
       </SelectStyle>
     </>
@@ -16,8 +21,10 @@ const Select = ({ id, option, ...restProps }) => {
 }
 
 Select.propTypes = {
-  /** 셀렉트의 옵션 값을 배열로 설정 할 수 있습니다. */
+  /** 셀렉트의 옵션 값을 설정 할 수 있습니다. */
   option: array.isRequired,
+  selected: number,
+  /**  */
   id: string.isRequired,
 }
 
