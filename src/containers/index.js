0f73edf7a92b13c16872f52108dd7f1a47f0ca7c
Hanna Kim <shhn0509@gmail.com>
2021-03-31 08:07:01 +0900

    [구성] containers 앤트리 파일 설정

---
+++ src/containers/index.js
@@ -0,0 +1,4 @@
+export { default as FAQ } from './FAQ/FAQ'
+export { default as Merit } from './Merit/Merit'
+export { default as OurStory } from './OurStory/OurStory'
+export { default as Promotion } from './Promotion/Promotion'
