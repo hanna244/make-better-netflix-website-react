20fa88659e70985fbc92214e8a408049aebf3cd8
Hanna Kim <shhn0509@gmail.com>
2021-03-30 15:50:14 +0900

    [문제] Unexpected keyword 오류 발생
    상황 : 로컬 데이터를 동적으로 불러오려는데 default 키워드에 문제 발생

--- src/containers/OurStory/OurStory.js
+++ src/containers/OurStory/OurStory.js
@@ -1,8 +1,38 @@
-import React from 'react'
-import { Defualt } from '../../layout/LayoutTypes/Defualt'
+import React, { useState, useEffect } from 'react'
+import { Background, Container } from '../../components'
+import { Promotion } from '../index'
+import { SectionStyle } from './OurStory.style'
 
-const OurStory = () => {
-  return <Defualt></Defualt>
+const OurStory = ({ headingLevel }) => {
+  const [homeData, setHomeData] = useState({})
+  const fetchData = () => {
+    return import('../../data/homeContext.json')
+      .then((data) => {
+        // console.log(data)
+        const { default } = data
+        setHomeData(default)
+      })
+      .catch((error) => console.error('데이터를 불러오지 못했습니다.'))
+  }
+  // const {default} = data
+  // setHomeData(default)
+
+  useEffect(() => {
+    fetchData()
+    console.log(homeData)
+  }, [homeData])
+
+  return (
+    <Container>
+      <SectionStyle>
+        <h2 as={headingLevel}></h2>
+        <p></p>
+        <p></p>
+        <p></p>
+        <Promotion />
+      </SectionStyle>
+    </Container>
+  )
 }
 
 export default OurStory
