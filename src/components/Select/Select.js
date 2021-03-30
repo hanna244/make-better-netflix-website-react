67d027c4b31eeb63e5d17cdd4a3a5248d8ba247e
Hanna Kim <shhn0509@gmail.com>
2021-03-30 10:51:37 +0900

    [문제] 리스트 렌더링 시 map을 찾을 수 없다는 오류 발생

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import { v4 as uuid } from 'uuid'
+import { bool, string, array } from 'prop-types'
 
-const Select = ({}) => {}
+const Select = ({ id, optionList, labelHidden, ...restProps }) => {
+  return (
+    <Fragment>
+      <label labelHidden={labelHidden} htmlFor={id}></label>
+      <select id={id} {...restProps}>
+        {optionList.map((item) => (
+          <option key={uuid()} id={uuid()} value={item.value}>
+            {item.list}
+          </option>
+        ))}
+      </select>
+    </Fragment>
+  )
+}
 
-Select.propTypes = {}
+Select.propTypes = {
+  id: string.isRequired,
+  optionList: array,
+  labelHidden: bool,
+}
 
 export default Select
