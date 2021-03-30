7a905c3efc377a89f3fe52cab0988e09b0496ef2
Hanna Kim <shhn0509@gmail.com>
2021-03-30 17:02:30 +0900

    [문제 해결] 데이터 접근 문제 해결
    원인: 데이터를 구조 분해 할당하는 시점에 문제가 있었다.
    해결: then() 내부에서 구조 분해 할달을 하니 문제 해결

--- src/containers/OurStory/OurStory.js
+++ src/containers/OurStory/OurStory.js
@@ -5,17 +5,12 @@ import { SectionStyle } from './OurStory.style'
 
 const OurStory = ({ headingLevel }) => {
   const [homeData, setHomeData] = useState({})
-  // koHomeData에 접근 불가능 ? 왜 ?
-  // https://shhn0509.gitbook.io/netflix-website-react/undefined-3/error-notes#6
-  const { ko: koHomeData } = homeData[0]
-  console.log(koHomeData)
-  // console.log(homeData[0])
-  // const { title, strongText, signUp, promotion } = koHomeData
   const fetchData = () => {
     return import('../../data/homeContext.json')
       .then((data) => {
         const { default: _default } = data
-        setHomeData(_default)
+        const { ko: koHomeData } = _default[0]
+        setHomeData(koHomeData)
       })
       .catch((error) => console.error('데이터를 불러오지 못했습니다.'))
   }
@@ -27,10 +22,10 @@ const OurStory = ({ headingLevel }) => {
   return (
     <Container>
       <SectionStyle dim>
-        {/* <h2 as={headingLevel}>{title}</h2>
-        <p>{strongText}</p>
-        <p>{signUp}</p>
-        <p>{promotion}</p> */}
+        <h2 as={headingLevel}>{homeData.title}</h2>
+        <p>{homeData.strongText}</p>
+        <p>{homeData.signUp}</p>
+        <p>{homeData.promotion}</p>
         <Promotion />
       </SectionStyle>
     </Container>
