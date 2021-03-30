f444df38965efbf550fd99890a06e2a81e023bd7
Hanna Kim <shhn0509@gmail.com>
2021-03-30 14:43:30 +0900

    [구성] Defualt 컴포넌트 활성화

--- src/layout/LayoutTypes/Dufualt.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Header } from '../Header/Header'
-import { Footer } from '../Footer/Footer'
-
-export const Dufualt = ({ children, ...restProps }) => {
-  return <>{/* <Header />
-      {children}
-      <Footer /> */}</>
-}
