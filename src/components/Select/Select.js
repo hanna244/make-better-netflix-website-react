a89b90b0c09938fab51e1a9276ff17b2daf5ace4
Hanna Kim <shhn0509@gmail.com>
2021-03-31 08:06:26 +0900

    [구성] Select → propTypes 설정 및 주석 추가

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -21,8 +21,13 @@ const Select = ({ id, optionList = [], a11yLabel, label, ...restProps }) => {
 }
 
 Select.propTypes = {
+  /** 접근성을 위해 레이블을 설정합니다. */
+  label: string.isRequired,
+  /** 접근성을 위해 레이블과 셀렉트의 id를 설정합니다. */
   id: string.isRequired,
+  /** option에 전달하는 값은 배열 유형으로 전달합니다. 배열에는 label과 value가 포함되어야 합니다. */
   optionList: array,
+  /** 레이블을 CSS로 감춤 처리 할 수 있습니다. */
   a11yLabel: bool,
 }
 
