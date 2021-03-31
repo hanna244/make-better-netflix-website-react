612d471082a954ab5888848be2324346406a714f
Hanna Kim <shhn0509@gmail.com>
2021-03-31 17:16:52 +0900

    [경험] Promotion → input (in)valid 조건 처리 오류 해결 시도

--- src/containers/Promotion/Promotion.js
+++ src/containers/Promotion/Promotion.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useCallback } from 'react'
 import { isValidEmail } from '../../utils'
 import {
   PromotionContainer,
@@ -9,24 +9,43 @@ import {
 const Promotion = ({ className, ...restProps }) => {
   // Input 컴포넌트에서 value 값을 가져오기 위해 사용
   const [inputValue, setInputValue] = useState('')
-  const [inputAttr, setInputAttr] = useState({})
+  const [inputAttr, setInputAttr] = useState({ valid: false, invalid: false })
 
   // const passwordRegEx = /^[0-9].{8,60}$/
   console.log(isValidEmail(inputValue))
 
-  const handleInputIsValid = () => {
-    if (inputValue.trim().length !== 0) {
-      setInputAttr({ valid: `valid="true"` })
+  const checkEmail = useCallback((value) => {
+    if (!isValidEmail(value)) {
+      setInputAttr({ invalid: true })
     } else {
-      setInputAttr({ valid: `valid="false"` })
+      setInputAttr({ invalid: false })
     }
+  }, [])
+
+  const checkValueLength = useCallback((value) => {
+    if (value.trim().length === 0) {
+      setInputAttr({ valid: false })
+    } else {
+      setInputAttr({ valid: true })
+    }
+  }, [])
+
+  const handleInputIsValid = () => {
+    checkValueLength(inputValue)
+    checkEmail(inputValue)
+
+    // if (!isValidEmail(inputValue)) {
+    //   setInputAttr({ invalid: true })
+    // } else {
+    //   setInputAttr({ invalid: false })
+    // }
     // 에러 발생. isValidEmail(inputValue)의 결과는 true가 나왔지만 아래 오류가 떠서 invalid="true"가 된다.
     // index.js:1 Warning: Failed prop type: Invalid prop `invalid` of type `string` supplied to `Input`, expected `boolean`.
-    if (!isValidEmail(inputValue)) {
-      setInputAttr({ invalid: `invalid="true"` })
-    } else {
-      setInputAttr({ invalid: `invalid="false"` })
-    }
+    // if (inputValue.trim().length === 0) {
+    //   setInputAttr({ valid: false })
+    // } else {
+    //   setInputAttr({ valid: true })
+    // }
   }
 
   return (
