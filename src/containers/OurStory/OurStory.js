426f5211e9e5ec3c398a0cc12fdc1eddb2ca9818
Hanna Kim <shhn0509@gmail.com>
2021-03-31 15:12:35 +0900

    [구성] OurStory → 컴포넌트 다시 구성
    이전의 컴포넌트 복잡성에 의해 오류가 자주 발생, 다시 만들어보기

--- src/containers/OurStory/OurStory.js
+++ src/containers/OurStory/OurStory.js
@@ -1,7 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
+import { Promotion } from '..'
+import { Background, Container } from '../../components'
+import { Head } from './OurStory.style'
 
-const OurStory = () => {
-  return <div>입력하세요</div>
+const OurStory = ({ headingLevel, ...restProps }) => {
+  const fetchData = useCallback(() => {
+    import('../../data/homeContext.json').then((module) => {
+      const { default: _default } = module
+      const { ko: koHomeData } = _default[0]
+      setHomeContent(koHomeData)
+    })
+  }, [])
+
+  const [homeContent, setHomeContent] = useState({})
+  const { title, subTitle, signUp, promotionInfo } = homeContent
+
+  useEffect(() => {
+    fetchData()
+  }, [fetchData])
+
+  return (
+    <Background as="section" {...restProps}>
+      <Container>
+        <Head as={headingLevel}>넷플릭스 소개</Head>
+        <p>{title}</p>
+        <p>{subTitle}</p>
+        <p>{signUp}</p>
+        <Promotion />
+        <p>{promotionInfo}</p>
+      </Container>
+    </Background>
+  )
 }
 
 export default OurStory
---
+++ src/containers/OurStory/OurStory.style.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+import { a11yHidden } from '../../styles/common'
+
+export const Head = styled.h2`
+  ${a11yHidden}
+`
+
+export const TitleStyled = styled.p``
+export const SubTitleStyled = styled.p``
+export const SignUpStyled = styled.p``
+export const PromotionInfoStyled = styled.p``
