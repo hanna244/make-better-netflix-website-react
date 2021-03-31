ec82148e49f432afc1128327dbac26a412817359
Hanna Kim <shhn0509@gmail.com>
2021-03-31 14:27:14 +0900

    [문제 해결] Container → children 유형 변경
    상황 : children 속성의 유형을 string으로 설정했더니 오류 발생
    원인 : JSX 요소는 배열로 설정된다. 그렇기 때문에 string으로 설정하면
    안됨.

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,25 +1,18 @@
 import React from 'react'
 import { ContainerStyle } from './Container.style'
-// import Logo from '../Logo/Logo'
-import { object, oneOf, string } from 'prop-types'
+import { object, oneOf } from 'prop-types'
 
-const Container = ({ align, style, children, ...restProps }) => {
+const Container = ({ align, style, ...restProps }) => {
   const styles = {
     textAlign: align,
     ...style,
   }
 
-  return (
-    <ContainerStyle style={styles} {...restProps}>
-      {children}
-    </ContainerStyle>
-  )
+  return <ContainerStyle style={styles} {...restProps} />
 }
 
 Container.defaultProps = {
   children: '컨텐츠를 넣어주세요',
-
-  // children: <Logo width="350" />,
 }
 
 Container.propTypes = {
@@ -27,8 +20,6 @@ Container.propTypes = {
   align: oneOf(['center', 'left', 'right']),
   /** 사용자 정의 스타일을 객체 타입으로 설정할 수 있습니다. */
   style: object,
-  /** 내부 컨텐츠를 설정할 수 있습니다. */
-  children: string,
 }
 
 export default Container
