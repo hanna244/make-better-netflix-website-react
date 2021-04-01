83405a3b104745e243c32ffab9994a7e98816580
Hanna Kim <shhn0509@gmail.com>
2021-04-02 07:48:07 +0900

    [변경] Promotion → react-redux 적용을 위한 기존의 코드 정리
    react-redux로 상태관리 하기 위해 기존에 있던 useState로 관리하던
    상태 코드 정리

--- src/containers/Promotion/Promotion.js
+++ src/containers/Promotion/Promotion.js
@@ -1,4 +1,5 @@
 import React, { useState, useCallback } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import { isValidEmail } from '../../utils'
 import {
   PromotionContainer,
@@ -7,26 +8,6 @@ import {
 } from './Promotion.style'
 
 const Promotion = ({ className, ...restProps }) => {
-  // Input 컴포넌트에서 value 값을 가져오기 위해 사용
-  const [inputValue, setInputValue] = useState('')
-  const [inputAttr, setInputAttr] = useState({ valid: false, invalid: false })
-
-  // const passwordRegEx = /^[0-9].{8,60}$/
-  console.log(isValidEmail(inputValue))
-
-  const handleInputIsValid = useCallback(() => {
-    const valueLength = inputValue.trim().length
-    const checkEmail = isValidEmail(inputValue)
-
-    if (!checkEmail && valueLength === 0) {
-      setInputAttr({ valid: false, invalid: false })
-    } else if (!checkEmail && valueLength !== 0) {
-      setInputAttr({ valid: true, invalid: true })
-    } else if (checkEmail && valueLength !== 0) {
-      setInputAttr({ valid: true, invalid: false })
-    }
-  }, [inputValue, setInputAttr])
-
   return (
     <PromotionContainer className={className} {...restProps}>
       <PromotionInput
@@ -34,9 +15,9 @@ const Promotion = ({ className, ...restProps }) => {
         type="email"
         label="이메일 주소"
         errorMessege="정확한 이메일 주소를 입력하세요."
-        onInputComplete={(value) => setInputValue(value)}
-        onBlur={handleInputIsValid}
-        {...inputAttr}
+        // onInputComplete={(value) => setInputValue(value)}
+        // onBlur={handleInputIsValid}
+        // {...inputAttr}
       />
       <SignUpButton label="30일 무료 이용" />
     </PromotionContainer>
