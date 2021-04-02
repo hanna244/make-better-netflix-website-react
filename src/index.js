408edd854a9c37307f6b5506ef817e0bda88a926
Hanna Kim <shhn0509@gmail.com>
2021-04-02 17:12:35 +0900

    [구성] json 데이터 context aip 사용해서 컴포넌트에 공급

--- src/index.js
+++ src/index.js
@@ -4,16 +4,33 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { store } from './store/store'
 
+import { DataContext } from './context/context'
+import addressData from './data/address.json'
+import faqData from './data/faq.json'
+import footerLinkData from './data/footerLink.json'
+import homeContextData from './data/homeContext.json'
+import meritData from './data/merit.json'
+
 import { GlobalStyles } from './styles/GlobalStyles'
 import './styles/normalize.css'
 import App from './App/App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <GlobalStyles />
-      <App />
-    </Provider>
+    <DataContext.Provider
+      value={{
+        addressData: addressData,
+        faqData: faqData,
+        footerLinkData: footerLinkData,
+        homeContextData: homeContextData,
+        meritData: meritData,
+      }}
+    >
+      <Provider store={store}>
+        <GlobalStyles />
+        <App />
+      </Provider>
+    </DataContext.Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
