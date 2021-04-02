ffcd1bf52a2ef496c8f7b5cf9c6ddfd1ef737b5a
Hanna Kim <shhn0509@gmail.com>
2021-04-02 17:17:36 +0900

    [구성] Home → HomeMerit 컴포넌트 바인딩

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,12 @@
 import { useCallback } from 'react'
 import { Defualt } from '../../layout/LayoutTypes/Defualt'
-import { OurStory, Merit, FAQ } from '../../containers/index'
+import { OurStory, HomeMerit, FAQ } from '../../containers/index'
 
 function Home() {
   return (
     <Defualt className="home">
       <OurStory />
-      <Merit />
+      <HomeMerit />
       <FAQ />
     </Defualt>
   )
