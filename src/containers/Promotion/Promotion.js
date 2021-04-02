f4f6fcec5bae0a99019b852a01f5a9d55979a438
Hanna Kim <shhn0509@gmail.com>
2021-04-02 13:05:56 +0900

    [변경] inputslice 상태와 액션 위치 변경

--- src/containers/Promotion/Promotion.js
+++ src/containers/Promotion/Promotion.js
@@ -1,22 +1,42 @@
-import React, { useState, useCallback } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { isValidEmail } from '../../utils'
+import React, { useCallback } from 'react'
 import {
   PromotionContainer,
   PromotionInput,
   SignUpButton,
 } from './Promotion.style'
+import { useSelector, useDispatch } from 'react-redux'
+import { emailValueUpdate } from '../../store/slices/inputslice'
 
-const Promotion = ({ className, ...restProps }) => {
+const Promotion = ({ id, name, className, ...restProps }) => {
+  // 스토어에서 상태 가져오기
+  const { inputValue } = useSelector(({ formReducer }) => {
+    return {
+      inputValue: formReducer.email,
+    }
+  })
+
+  // 스토어에서 액션 가져오기
+  const dispatch = useDispatch()
+
+  const updateValue = useCallback(() => dispatch(emailValueUpdate()), [])
+
+  const handleChange = useCallback(
+    (e) => {
+      updateValue(e.target.value)
+    },
+    [updateValue]
+  )
+
   return (
     <PromotionContainer className={className}>
       <PromotionInput
         type="email"
         label="이메일 주소"
+        name="email"
+        id="useEmail"
         errorMessege="정확한 이메일 주소를 입력하세요."
-        // onInputComplete={(value) => setInputValue(value)}
-        // onBlur={handleInputIsValid}
-        // {...inputAttr}
+        value={inputValue}
+        handleChange={handleChange}
         {...restProps}
       />
       <SignUpButton label="30일 무료 이용" />
