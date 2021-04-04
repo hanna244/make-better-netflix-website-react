515ac34de0a3acf05944c2d9d5bcfae88a9ff3a3
Hanna Kim <shhn0509@gmail.com>
2021-04-05 03:53:53 +0900

    [개선] faqData 비동기 통신으로 받아오기

--- src/containers/FAQ/FAQ.js
+++ src/containers/FAQ/FAQ.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useCallback, useState, Fragment } from 'react'
 import { v4 as uuid } from 'uuid'
 import { Accordion } from '../../components'
-import faqData from '../../data/faq.json'
 
 import { useSelector, useDispatch } from 'react-redux'
 import { currentNumber, accordionIsOpen } from '../../store/slices/faqslice'
@@ -9,26 +8,29 @@ import { currentNumber, accordionIsOpen } from '../../store/slices/faqslice'
 import { List } from './FAQ.style'
 
 const FAQ = ({ as, label, children, ...restProps }) => {
-  // const fetchData = useCallback(() => {
-  //   import('../../data/faq.json')
-  //     .then((data) => {
-  //       const { default: _default } = data
-  //       setFaqData(_default)
-  //     })
-  //     .catch((error) => console.error('데이터를 받아오는데 실패했습니다.'))
-  // })
+  // 데이터 비동기 통신으로 받아오기
+  const [faqData, setFaqData] = useState([])
+  const fetchData = useCallback(() => {
+    import('../../data/faq.json')
+      .then((data) => {
+        const { default: _default } = data
+        setFaqData(_default)
+      })
+      .catch((error) => console.error('데이터를 받아오는데 실패했습니다.'))
+  }, [])
 
-  // useEffect(() => {
-  //   fetchData()
-  // }, [fetchData])
+  useEffect(() => {
+    fetchData()
+  }, [fetchData])
 
+  // 스토어에서 상태 가져오기
   const { currentIndex, isOpen } = useSelector((state) => {
     return {
       currentIndex: state.curious.currentIndex,
       isOpen: state.curious.isOpen,
     }
   })
-
+  // dispatch를 통해 액션 실행
   const dispatch = useDispatch()
   const changeIndex = (index) => dispatch(currentNumber(index))
   const changeOpenState = () => dispatch(accordionIsOpen())
