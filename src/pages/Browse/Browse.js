67a2c9d00c74d99b87d684c42af96674d05fbbb9
Hanna Kim <shhn0509@gmail.com>
2021-04-12 14:16:15 +0900

    [문제] Browse 라우터 설정 시 중첩 된 형태로 나타남.
    원인: 중첩 라우터에 대한 이해 부족. 중첩 라우팅 시 매인 페이지의
    콘텐츠가 중첩되어 보이는 것이 당연하다.

--- src/pages/Browse/Browse.js
+++ src/pages/Browse/Browse.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import { BrowseType } from '../../layout/LayoutTypes'
+import { Route, Switch } from 'react-router-dom'
+import { TV, Movie, MyList } from '.'
 
 const Browse = () => {
   return (
     <BrowseType>
       <div style={{ fontSize: '50px' }}>브라우즈 페이지 입니다.</div>
+      <Switch>
+        <Route path="/browse/genre/tv" component={TV} />
+        <Route path="/browse/genre/movie" component={Movie} />
+        <Route path="/browse/mylist" component={MyList} />
+      </Switch>
     </BrowseType>
   )
 }
--- src/pages/Browse/index.js
+++ src/pages/Browse/index.js
@@ -1,3 +1,4 @@
 export { default as TV } from './Genre/TV'
 export { default as Movie } from './Genre/Movie'
 export { default as MyList } from './MyList/MyList'
+export { default as Browse } from './Browse'
