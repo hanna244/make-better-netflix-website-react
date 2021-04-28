65dde67c7e0ee0f68859ab8e63321d313db212ad
Hanna Kim <shhn0509@gmail.com>
2021-04-28 16:40:48 +0900

    [변경] Accordion, FAQ → 코드 되돌리기
    - Accordion에 적용된 map() 순환 설정을 제거하기 위한 코드 되돌리기
    - 원인: 컴파운드 컴포넌트는 해당 컴포넌트 내부에서 조립하는 것이 아닌
    외부(container)에서 조립하여 사용한다.

--- src/components/Accordion/Accordion.js
+++ src/components/Accordion/Accordion.js
@@ -1,122 +1,76 @@
-import React, { createContext, Fragment, useContext } from 'react'
-import { array } from 'prop-types'
+import React from 'react'
+import { string, any } from 'prop-types'
 import { Item, Head, Body, OpenButton, PlusImg } from './Accordion.style'
 import { motion, AnimatePresence } from 'framer-motion'
 import { getPublicAssets } from 'utils'
 
-const AccordionContext = createContext()
-
 const Accordion = ({
-  data,
-  handleClick,
+  id,
+  isVisible,
+  question,
+  answer,
+  handleToggle,
   currentIndex,
   isOpen,
   ...restProps
 }) => {
-  // console.log(currentIndex)
   return (
-    <AccordionContext.Provider
-      value={{ data, handleClick, currentIndex, isOpen }}
-    >
-      {data.map((item, index) => (
-        <Accordion.Item
-          question={item.question}
-          answer={item.answer}
-          key={`Accordion_${index}`}
-          id={`Accordion_${index}`}
-          index={index}
-          currentIndex={currentIndex}
-          isOpen={isOpen}
-          {...restProps}
-        />
-      ))}
-    </AccordionContext.Provider>
-  )
-}
-
-Accordion.defaultProps = {
-  data: [],
-}
-Accordion.propTypes = {
-  data: array,
-}
-
-/* Accordion 컴파운드 컴포넌트 ------------------------------------------------------ */
-
-Accordion.Item = function AccordionItem({
-  question,
-  answer,
-  index,
-  ...restProps
-}) {
-  const context = useContext(AccordionContext)
-  const { handleClick, currentIndex, isOpen } = context
-  console.log(index)
-  return (
-    <Item onClick={handleClick}>
-      <Accordion.Head {...restProps} children={question} />
-      <Accordion.Body
-        index={index}
-        currentIndex={currentIndex}
-        children={answer}
-        isOpen={isOpen}
-      />
+    <Item className="accordionItem" {...restProps}>
+      <Head>
+        {question}
+        <OpenButton>
+          <PlusImg
+            width="40"
+            height="40"
+            src={`${getPublicAssets('plusIcon.svg')}`}
+            alt="답변 보기"
+          />
+        </OpenButton>
+      </Head>
+      <AnimatePresence>
+        {isOpen && currentIndex === id && (
+          <Body
+            as={motion.dd}
+            // 애니메이션의 기본 요소 스타일
+            initial={{ height: 0 }}
+            // 애니메이션이 적용 되었을 때 요소의 스타일
+            animate={{ height: 'auto' }}
+            // 애니메이션이 끝날 때 요소의 스타일
+            exit={{ height: 0 }}
+            // 애니메이션 트렌지션
+            transition={{
+              type: 'spring',
+              // 애니메이션 지연시간
+              duration: 0.3,
+            }}
+          >
+            {answer.map((item, index) => {
+              return (
+                <p
+                  key={`Accordion_Body_${index}`}
+                  id={`Accordion_Body_${index}`}
+                >
+                  {item}
+                </p>
+              )
+            })}
+          </Body>
+        )}
+      </AnimatePresence>
     </Item>
   )
 }
 
-Accordion.Head = function AccordionHead({ index, children, ...restProps }) {
-  return (
-    <Head {...restProps}>
-      {children}
-      <OpenButton data-index={index}>
-        <PlusImg
-          width="40"
-          height="40"
-          src={`${getPublicAssets('plusIcon.svg')}`}
-          alt="답변 보기"
-        />
-      </OpenButton>
-    </Head>
-  )
+Accordion.defaultProps = {
+  question: '이곳에 질문을 입력하세요.',
+  answer: [],
 }
 
-Accordion.Body = function AccordionBody({ children, index, ...restProps }) {
-  const context = useContext(AccordionContext)
-  const { isOpen, currentIndex } = context
-  // console.log(index)
-  // console.log(currentIndex)
-
-  return (
-    <AnimatePresence>
-      {isOpen && currentIndex === index && (
-        <Body
-          as={motion.dd}
-          // 애니메이션의 기본 요소 스타일
-          initial={{ height: 0 }}
-          // 애니메이션이 적용 되었을 때 요소의 스타일
-          animate={{ height: 'auto' }}
-          // 애니메이션이 끝날 때 요소의 스타일
-          exit={{ height: 0 }}
-          // 애니메이션 트렌지션
-          transition={{
-            type: 'spring',
-            // 애니메이션 지연시간
-            duration: 0.3,
-          }}
-          {...restProps}
-        >
-          {children.map((item, index) => {
-            return (
-              <p key={`Accordion_Body_${index}`} id={`Accordion_Body_${index}`}>
-                {item}
-              </p>
-            )
-          })}
-        </Body>
-      )}
-    </AnimatePresence>
-  )
+Accordion.propTypes = {
+  /** 아코디언 메뉴의 헤더에 사용자 정의 질문을 설정할 수 있습니다. */
+  question: string,
+  /** 아코디언 메뉴의 바디에 사용자 정의 답변을 설정할 수 있습니다. */
+  answer: any,
 }
 
 export default Accordion
--- src/containers/FAQ/FAQ.js
+++ src/containers/FAQ/FAQ.js
@@ -6,7 +6,6 @@ import { currentNumber } from 'store/slices/faqslice'
 import { List } from './FAQ.style'
 
 const FAQ = ({ as, label, children, ...restProps }) => {
-  const [isOpen, setIsOpen] = useState(false)
   // 데이터 비동기 통신으로 받아오기
   const [faqData, setFaqData] = useState([])
   const fetchData = useCallback(() => {
@@ -32,24 +31,25 @@ const FAQ = ({ as, label, children, ...restProps }) => {
   const dispatch = useDispatch()
   const changeIndex = (index) => dispatch(currentNumber(index))
 
-  const handleAccordionStateandIndex = (e) => {
-    let dataIndex = e.target.getAttribute('data-index')
-    let compareIndex = dataIndex === currentIndex
-    changeIndex(compareIndex ? null : dataIndex)
-    setIsOpen(!compareIndex ? true : false)
-    console.log(currentIndex)
-    console.log(dataIndex)
-    console.log(e.target)
+  const handleAccordionStateandIndex = (index) => {
+    changeIndex(index === currentIndex ? null : index)
   }
+
   return (
     <Fragment>
       <List {...restProps}>
-        <Accordion
-          data={faqData}
-          handleClick={handleAccordionStateandIndex}
-          isOpen={isOpen}
-          currentIndex={currentIndex}
-        />
+        {faqData.map((item, index) => (
+          <Accordion
+            key={`Accordion_${index}`}
+            // Accordion open/close 조건 처리를 위해 Accordion_index가 아닌 index로 전달
+            id={index}
+            question={item.question}
+            answer={item.answer}
+            onClick={() => handleAccordionStateandIndex(index)}
+            currentIndex={currentIndex}
+            isOpen={index === currentIndex ? true : false}
+          />
+        ))}
       </List>
     </Fragment>
   )
