1f313bb0644deaaa5d61fadb11553f7917dc329b
Hanna Kim <shhn0509@gmail.com>
2021-05-20 16:02:03 +0900

    [문제] Navigation 컴포넌트 사용 시 linkTag prop 오류
    - linkTag="NavLink"를 전달했으나 브라우저에서 navlink로 뜸

--- src/layout/Header/NavHeader/NavHeader.js
+++ src/layout/Header/NavHeader/NavHeader.js
@@ -1,10 +1,15 @@
 import React, { useCallback } from 'react'
 import { NavLink, useHistory } from 'react-router-dom'
+import { Navigation } from 'components'
 import {
   OuterContainer,
   InnerContainer,
   NavHeaderHomeLink,
   NavHeaderLogo,
+  AriaStyle,
+  NavHeaderIconListStyle,
+  NavHeaderSearchIconStyle,
+  NavHeaderBellIconStyle,
 } from './NavHeader.style'
 
 const NavHeader = ({ className, hasLogInButton, ...restProps }) => {
@@ -25,12 +30,33 @@ const NavHeader = ({ className, hasLogInButton, ...restProps }) => {
             <NavHeaderLogo alt="브라우즈 홈" />
           </NavHeaderHomeLink>
         </h1>
-        <nav>
+        <Navigation
+          linkTag="NavLink"
+          linkArray={[
+            { to: `/browse`, label: '홈' },
+            { to: `/browse/genre/tv`, label: 'TV 프로그램' },
+            { to: `/browse/genre/movie`, label: '영화' },
+            { to: `/browse/mylist`, label: '내가 찜한 콘텐츠' },
+          ]}
+        />
+        {/* <nav>
           <NavLink to={`/browse`}>홈</NavLink>
           <NavLink to={`/browse/genre/tv`}>TV 프로그램</NavLink>
           <NavLink to={`/browse/genre/movie`}>영화</NavLink>
           <NavLink to={`/browse/mylist`}>내가 찜한 콘텐츠</NavLink>
-        </nav>
+        </nav> */}
+        <NavHeaderIconListStyle>
+          <li>
+            <NavHeaderSearchIconStyle type="button">
+              <AriaStyle>영화 검색</AriaStyle>
+            </NavHeaderSearchIconStyle>
+          </li>
+          <li>
+            <NavHeaderBellIconStyle type="button">
+              <AriaStyle>새로운 알림</AriaStyle>
+            </NavHeaderBellIconStyle>
+          </li>
+        </NavHeaderIconListStyle>
       </InnerContainer>
     </OuterContainer>
   )
--- src/layout/Header/NavHeader/NavHeader.style.js
+++ src/layout/Header/NavHeader/NavHeader.style.js
@@ -1,10 +1,25 @@
 import styled from 'styled-components'
+import { a11yHidden, resetButton, resetList } from 'styles/common'
+import { getPublicAssets } from 'utils'
 import {
   OuterContainerStyle as DefaultOuterContainer,
   InnerContainerStyle as DefaultInnerContainer,
   HeaderHomeLinkStyle as DefaultHeaderHomeLink,
   HeaderLogoStyle as DefaultHeaderLogo,
 } from '../DefaultHeader/DefaultHeader.style'
+
+// 네브 헤더 아이콘 공용 스타일 ----------------------------------------- //
+export const IconCommonStyle = styled.button`
+  ${resetButton}
+  background: inherit;
+
+  &::before {
+    content: '';
+    display: inline-block;
+    width: 2.1rem;
+    height: 2.1rem;
+  }
+`
 
 export const OuterContainer = styled(DefaultOuterContainer)``
 OuterContainer.displayName = 'OuterContainer'
@@ -21,3 +36,30 @@ NavHeaderHomeLink.displayName = 'NavHeaderHomeLink'
 
 export const NavHeaderLogo = styled(DefaultHeaderLogo)``
 NavHeaderLogo.displayName = 'NavHeaderLogo'
+
+export const AriaStyle = styled.span`
+  ${a11yHidden}
+`
+AriaStyle.displayName = 'AriaStyle'
+
+export const NavHeaderIconListStyle = styled.ul`
+  ${resetList}
+  display: flex;
+  flex-flow: row nowrap;
+`
+NavHeaderIconListStyle.displayName = 'NavHeaderIconListStyle'
+
+export const NavHeaderSearchIconStyle = styled(IconCommonStyle)`
+  &::before {
+    background: url(${getPublicAssets('search.svg')}) no-repeat;
+  }
+`
+NavHeaderSearchIconStyle.displayName = 'NavHeaderSearchIconStyle'
+
+export const NavHeaderBellIconStyle = styled(IconCommonStyle)`
+  &::before {
+    background: url(${getPublicAssets('bell.svg')}) no-repeat center/100%;
+    width: 2.2rem;
+  }
+`
+NavHeaderBellIconStyle.displayName = 'NavHeaderBellIconStyle'
