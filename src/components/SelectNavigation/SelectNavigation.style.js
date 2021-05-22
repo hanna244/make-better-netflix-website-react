426d315c20de6e74a428f2bf9f11eebd9b27561b
Hanna Kim <shhn0509@gmail.com>
2021-05-22 16:00:02 +0900

    [경험] Navigation, SelectNavigation 컴포넌트 제거
    - NavLink 사용 시 Router 미 포함으로 인한 설계 실수
    - NavHeader의 Navigation 구현 재시도를 위한 컴포넌트 제거

--- src/components/SelectNavigation/SelectNavigation.style.js
+++
@@ -1,7 +0,0 @@
-import { styled } from 'styled-components'
-
-export const SelectNavStyle = styled.nav``
-SelectNavStyle.displayName = 'SelectNavStyle'
-
-export const SelectNavLinkStyle = styled.nav``
-SelectNavLinkStyle.displayName = 'SelectNavLinkStyle'
